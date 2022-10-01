edd103d14d30a5ca2baeb00fa3a45595a29509ed
Ihor Volokhovych <ihor.volokhovych@pm.bet>
2022-10-01 21:11:13 +0300

    fixed glossary

--- DistEdu.GlossaryOfTerms/Program.cs
+++ DistEdu.GlossaryOfTerms/Program.cs
@@ -2,6 +2,9 @@
 
 using DistEdu.GlossaryOfTerms;
 
+await Reader.ProcessValues("TestFiles");
+await Task.WhenAll(Reader.WriteCustomFileAsync(), Reader.WriteJsonFileAsync(), Reader.WriteMsgPackFileAsync());
+
 // For csv.
 await Reader.ProcessCsvValues("TestFiles");
 // await Task.WhenAll(Reader.WriteCustomFileAsync(), Reader.WriteJsonFileAsync(), Reader.WriteMsgPackFileAsync());
