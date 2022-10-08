334eec92a0f06ffaf4a07888aedc8af73577625e
Ihor Volokhovych <ihor.volokhovych@pm.bet>
2022-10-09 01:10:27 +0300

    implemented index

---
+++ DistEdu.CoordinatedIndex/Key.cs
@@ -0,0 +1,37 @@
+using Lucene.Net.Index;
+using Lucene.Net.Linq.Mapping;
+using Lucene.Net.Search;
+
+namespace DistEdu.CoordinatedIndex;
+
+internal class Key : IDocumentKey
+{
+    public string Id { get; }
+    public Key(string id)
+    {
+        Id = id;
+    }
+
+
+    public bool Equals(IDocumentKey other)
+    {
+        var otherPie = other as Key;
+        if (otherPie != null)
+        {
+            return otherPie.Id == this.Id;
+        }
+        return false;
+    }
+
+
+    public Query ToQuery()
+    {
+        return new TermQuery(new Term("__key", this.Id));
+    }
+
+    public bool Empty => false;
+
+    public IEnumerable<string> Properties => Enumerable.Empty<string>();
+
+    public object this[string property] => null;
+}
---
+++ DistEdu.CoordinatedIndex/KeyExtension.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+
+namespace DistEdu.CoordinatedIndex;
+
+public static class KeyExtension
+{
+    static ConditionalWeakTable<object, string> KeyTable = new();
+    internal static string Key(this object obj)
+    {
+        return KeyTable.GetValue(obj, o => Guid.NewGuid().ToString());
+    }
+    internal static string Key(this object obj, string key)
+    {
+        return KeyTable.GetValue(obj, o => key);
+    } 
+}
